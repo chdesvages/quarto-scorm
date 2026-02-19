61503b4821559a5559c39579d7c5502765c2e1c2
Charlotte Desvages <charlotte.desvages@ed.ac.uk>
2026-02-19 15:11:03 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Generate SCORM package from static HTML site
+
+This is a basic bash script to generate a bare-bones functional SCORM package to upload a static HTML site onto a VLE. This can be used e.g. with static sites rendered by Quarto or BookML.
+
+An example is included, using [this single-page Quarto site](https://chdesvages.github.io/quarto-display-settings/).
---
+++ generate_scorm.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+#
+# Example usage: run this in the root directory for your course if your HTML site
+# is in the _site/ subfolder:
+#
+# bash generate_scorm.sh _site my_course
+#
+# This will build a SCORM manifest and zip the contents of the _site/ folder
+# into a file called SCORM_my_course.zip
+# Note that your site needs to be generated/rendered first.
+
+# Default root directory is ./
+ROOT_DIR=${1:-"."}
+
+# Default output filename will be SCORM_main.zip
+OUTPUT_FILENAME=${2:-"main"}
+
+# Default unique identifier
+
+TEMPLATE_LINE="                <file href=\"FILE_PATH\"/>"
+ALL_LINES=$(cat scorm_manifest_template.xml)
+
+# Loop over the output of find, which lists all the files in root directory and subdirectories
+while IFS= read -r filename; do
+    # Build the corresponding line to include each file
+    FILENAME_NO_ROOT=${filename#"$ROOT_DIR/"}
+    XML_LINE="${TEMPLATE_LINE/FILE_PATH/$FILENAME_NO_ROOT}"
+    ALL_LINES+=$'\n'"$XML_LINE"
+done < <(find "$ROOT_DIR" -type f)
+
+# Close the last XML tags
+ALL_LINES+=$'\n'"        </resource>"
+ALL_LINES+=$'\n'"    </resources>"
+ALL_LINES+=$'\n'"</manifest>"
+
+# Display the output to the console
+printf $ALL_LINES
+
+# Write to manifest file
+MANIFEST_FILENAME="${ROOT_DIR}/imsmanifest.xml"
+echo "$ALL_LINES" > "$MANIFEST_FILENAME"
+# cat $MANIFEST_FILENAME
+
+# Zip the SCORM
+zip -r SCORM_"${OUTPUT_FILENAME}".zip "$ROOT_DIR"
